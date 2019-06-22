9363dfdc268df4a83f96f5dcdc7d330d0cbee6d0
Hector Morales veloz <hector@Hectors-MacBook-Pro.local>
2019-06-22 00:59:56 -0400

    Fix the swoosh label to be position in the center of any given screen

--- App-Swoosh/App-Swoosh/ViewController.swift
+++ App-Swoosh/App-Swoosh/ViewController.swift
@@ -13,8 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        swooshIco.frame = CGRect(x: view.frame.size.width / 2 - swooshIco.frame.size.width / 2, y: 50, width: swooshIco.frame.size.width, height: swooshIco.frame.size.height);
+        
+        bgImg.frame = view.frame;
     }
 
-
+    @IBOutlet weak var swooshIco: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
 }
 
