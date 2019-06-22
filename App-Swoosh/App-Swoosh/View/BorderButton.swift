330b0b2da6e81e090b3206a99231e4b969078fc8
Hector Morales veloz <hector@Hectors-MacBook-Pro.local>
2019-06-22 00:09:24 -0400

    Create a new project, add some layout and buttons for the first time

---
+++ App-Swoosh/App-Swoosh/View/BorderButton.swift
@@ -0,0 +1,19 @@
+//
+//  BorderButton.swift
+//  App-Swoosh
+//
+//  Created by Hector Morales veloz on 6/21/19.
+//  Copyright © 2019 Hector Morales veloz. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib();
+        layer.borderWidth = 3.0;
+        layer.borderColor = UIColor.white.cgColor;
+    }
+ 
+}
---
+++ App-Swoosh/App-Swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App-Swoosh
+//
+//  Created by Hector Morales veloz on 6/21/19.
+//  Copyright © 2019 Hector Morales veloz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
